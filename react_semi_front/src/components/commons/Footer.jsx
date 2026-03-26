bcbd256f7d6d53f0adc277b8efba8e95cfbc65f9
yglee <yglee0248@gmail.com>
2026-03-26 22:58:00 +0900

    어떠신가요 이건?
    header, footer, css, app css의 오타및 겹치는 부분 삭제등의 작업!

--- react_semi_front/src/components/commons/Footer.jsx
+++ react_semi_front/src/components/commons/Footer.jsx
@@ -4,22 +4,25 @@ import styles from "./Commons.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>
-        <div>
-          <p>팀 4코리아(유) </p>
-          <p>
-            서울특별시 팀4 팀4로, 팀4대로 팀4타워 77층 (77777) | 팀장: 이영민
-          </p>
-          <p>
-            사업자 등록번호: 010-9274-1409 | 개인정보관리책임자: 이영민 |
-            호스팅서비스사업자: 팀4코리아(유)
-          </p>
-          <p>고객센터: 9274-1409 | 이메일: yglee0248@gmail.com</p>
+      <div className={styles.footer_container}>
+        <div className={styles.company_info}>
+          <div className={styles.footer_logo}>C2C</div>
+          <div className={styles.info_text}>
+            <p>(주)씨투씨 | 대표: 누구게 | 사업자등록번호: 123-45-67890</p>
+            <p>서울특별시 에코구 자원순환대로 77, 1층 (지구동)</p>
+            <p>고객센터: 1588-0000 | 이메일: yglee0248@gmail.com</p>
+            <p>개인정보관리책임자: 누구게 | 호스팅서비스사업자: (주)씨투씨</p>
+            <p className={styles.copyright}>© 2026 C2C Inc. All rights reserved.</p>
+          </div>
         </div>
-        <div className={styles.sns}>
-          <span class="material-icons">language</span>
-          <span class="material-icons">language</span>
-          <span class="material-icons">language</span>
+
+        <div className={styles.sns_zone}>
+          <span className={styles.follow_text}>Follow us</span>
+          <div className={styles.sns_icons}>
+            <span className="material-icons">language</span>
+            <span className="material-icons">facebook</span>
+            <span className="material-icons">public</span>
+          </div>
         </div>
       </div>
     </footer>
--- react_semi_front/src/components/commons/Header.jsx
+++ react_semi_front/src/components/commons/Header.jsx
@@ -1,45 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Commons.module.css";
 import Button from "../ui/Button";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header className={styles.header}>
-      <div className={styles.main_header}>
+      <div className={styles.header_top}>
         <div className={styles.site_logo}>
           <Link to="/">C2C</Link>
         </div>
         <div className={styles.member_link_zone}>
           <Button
-            className="btn primary"
-            onClick={() => {
-              navigete("/member/login");
-            }}
+            className="btn outline"
+            onClick={() => navigate("/member/login")}
           >
             로그인
           </Button>
           <Button
             className="btn primary"
-            onClick={() => {
-              navigete("/member/join");
-            }}
+            onClick={() => navigate("/member/join")}
           >
             회원가입
           </Button>
         </div>
       </div>
-      <div className={styles.nav}>
-        <ul>
-          <Link to="/">
-            <li>중고거래</li>
-          </Link>
-          <Link to="/">
-            <li>커뮤니티</li>
-          </Link>
-          <Link to="/">
-            <li>API</li>
-          </Link>
-        </ul>
+
+      <div className={styles.header_bottom}>
+        <nav className={styles.nav}>
+          <ul>
+            <li><Link to="/market">중고거래</Link></li>
+            <li><Link to="/community">커뮤니티</Link></li>
+            <li><Link to="/map">우리동네 에코맵</Link></li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
