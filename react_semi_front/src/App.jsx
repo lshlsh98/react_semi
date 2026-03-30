45e74d37678eae93bd79bc4ca0cd0a71c0a864ad
comiNamuSoop <etudejh@naver.com>
2026-03-30 10:46:55 +0900

    마켓리스트페이지
    마켓리스트페이지

--- react_semi_front/src/App.jsx
+++ react_semi_front/src/App.jsx
@@ -1,25 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import "./assets/font/font.css";
 import Footer from "./components/commons/Footer";
 import Header from "./components/commons/Header";
-import Join from "./pages/member/Join";
-import Login from "./pages/member/Login";
-import Mypage from "./pages/Mypage";
+import MarketListPage from "./pages/market/MarketListPage";
 
 function App() {
   return (
-    <div className="wrap">
-      <Header />
-      <div className="main">
-        <Routes>
-          <Route path="/member/join" element={<Join />} />
-          <Route path="/member/login" element={<Login />} />
-          <Route path="/" />
-          <Route path="/member/mypage/*" element={<Mypage />} />
-        </Routes>
-      </div>
-      <Footer />
+    <div>
+      <Header></Header>
+      <div className="wrap"></div>
+      <Routes>
+        <Route path="/member/join" element={<Join />} />
+        <Route path="/member/login" element={<Login />} />
+
+        <Route path="/" />
+        <Route path="/member/mypage/*" element={<Mypage />} />
+
+        <Route path="/market" element={<MarketListPage />} />
+      </Routes>
+
+      <Footer></Footer>
     </div>
   );
 }
