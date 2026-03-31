0448c18f806f683fc0f844a31e1f3de140a2ebe2
lshlsh98 <lsh980324@naver.com>
2026-03-31 16:58:37 +0900

    임시

---
+++ react_semi_back/src/main/java/kr/co/iei/mypage/controller/MypageController.java
@@ -0,0 +1,13 @@
+package kr.co.iei.mypage.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(value="*")
+@RequestMapping(value="/mypages")
+@RestController
+public class MypageController {
+	
+			
+}
--- react_semi_front/src/components/mypage/MyBoardPage.jsx
+++ react_semi_front/src/components/mypage/MyBoardPage.jsx
@@ -3,11 +3,16 @@ import Pagination from "../ui/Pagination";
 import MyBoardList from "./board/MyBoardList";
 import styles from "./MyBoardPage.module.css";
 import BasicSelect from "../ui/BasicSelect";
+import useAuthStore from "../utils/useAuthStore";
 
 const MyBoardPage = () => {
+  const { memberId } = useAuthStore;
+
+  const [boardList, setBoardList] = useState([]);
   const [page, setPage] = useState(0);
-  const [totalPage, setTotalPage] = useState(10);
-  const [order, setOrder] = useState(0); // 0: 기본 1: 중급 2: 고급
+  const [size, setSize] = useState(10);
+  const [totalPage, setTotalPage] = useState(null);
+  const [order, setOrder] = useState(0); // 0: 최신순 / 1: 작성순 / 2: 조회수 / 3: 좋아요 / 4: 싫어요 / 5: 신고수
 
   return (
     <div className={styles.myboard_wrap}>
@@ -17,9 +22,12 @@ const MyBoardPage = () => {
             state={order}
             setState={setOrder}
             list={[
-              [0, "기본"],
-              [1, "중급"],
-              [2, "고급"],
+              [0, "최신순"],
+              [1, "작성순"],
+              [2, "조회수"],
+              [3, "좋아요"],
+              [4, "싫어요"],
+              [5, "신고수"],
             ]}
           />
         </div>
