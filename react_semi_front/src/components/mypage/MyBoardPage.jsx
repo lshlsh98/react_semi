e0867f564aad01b7f1f9b8cfd83cd67ece5feb1a
lshlsh98 <lsh980324@naver.com>
2026-03-31 17:50:09 +0900

    임시
    0550

--- react_semi_front/src/components/mypage/MyBoardPage.jsx
+++ react_semi_front/src/components/mypage/MyBoardPage.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pagination from "../ui/Pagination";
 import MyBoardList from "./board/MyBoardList";
 import styles from "./MyBoardPage.module.css";
 import BasicSelect from "../ui/BasicSelect";
 import useAuthStore from "../utils/useAuthStore";
+import axios from "axios";
 
 const MyBoardPage = () => {
   const { memberId } = useAuthStore;
@@ -13,6 +14,21 @@ const MyBoardPage = () => {
   const [size, setSize] = useState(10);
   const [totalPage, setTotalPage] = useState(null);
   const [order, setOrder] = useState(0); // 0: 최신순 / 1: 작성순 / 2: 조회수 / 3: 좋아요 / 4: 싫어요 / 5: 신고수
+
+  useEffect(() => {
+    axios
+      .get(
+        `${import.meta.env.VITE_BACKSERVER}/mypages?page=${page}&size=${size}&order=${order}`,
+      )
+      .then((res) => {
+        console.log(res);
+        // setBoardList(res.data.items);
+        // setTotalPage(res.data.totalPage);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [page, order]);
 
   return (
     <div className={styles.myboard_wrap}>
