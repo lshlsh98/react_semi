b5a2fc3c73a0f4265ca86906fbd47a728f9f1e6d
00chu <gamjachoi00@naver.com>
2026-04-01 17:05:32 +0900

    커뮤니티 목록 페이지 생성

--- react_semi_front/src/pages/community/CommunityListPage.jsx
+++ react_semi_front/src/pages/community/CommunityListPage.jsx
@@ -2,7 +2,7 @@ import styles from "./CommunityListPage.module.css";
 
 const CommunityListPage = () => {
   return (
-    <div>
+    <div className={styles.community_page_wrap}>
       <h3 className="page-title">커뮤니티</h3>
     </div>
   );
