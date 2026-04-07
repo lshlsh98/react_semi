6abd89c09389d8f3a0d4279d7e2f2641c30a3b3b
comiNamuSoop <etudejh@naver.com>
2026-04-07 15:38:05 +0900

    한진호 0407-1

--- react_semi_front/src/pages/market/MarketViewPage.jsx
+++ react_semi_front/src/pages/market/MarketViewPage.jsx
@@ -3,12 +3,19 @@ import Button from "../../components/ui/Button";
 import useAuthStore from "../../components/utils/useAuthStore";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { redirect, useParams } from "react-router-dom";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'; 
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ReportIcon from '@mui/icons-material/Report';
+import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
+
+
+
 const MarketViewPage = () => {
-  const { memberId } = useAuthStore();
+  const { memberId,isReady } = useAuthStore();
   //console.log(memberId);
 
   const params = useParams();
@@ -19,6 +26,10 @@ const MarketViewPage = () => {
   const imgUrl = "http://192.168.31.24:9999/market";
 
   useEffect(() => {
+    if(!isReady){
+      return;
+    }
+
     axios
       .get(`${import.meta.env.VITE_BACKSERVER}/markets/${marketNo}`)
       .then((res) => {
@@ -95,6 +106,7 @@ const MarketViewPage = () => {
                 <p>조회수 : {market.viewCount}</p>
                 <p>좋아요</p>
               </div>
+              <LikeAndReport market={marketNo} /> 
             </div>
             <div className={styles.title_map}>지도가 들어갈 예정</div>
             {memberId && (
@@ -126,5 +138,26 @@ const MarketViewPage = () => {
     </main>
   );
 };
+
+const LikeAndReport = ({marketNo})=>{
+  const[likeInfo,setLikeInfo] = useState(null);
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_BACKSERVER}/markets/${marketNo}/likes`)
+    .then((res)=>{
+      console.log(res)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  },[])
+  return(<> 
+  
+  {likeInfo &&(<> <ThumbUpAltIcon sx={{fill:"var(--primary)"}} />
+  <span style={{color:"var(--primary)"}}>10</span></>)}
+  
+  <ReportGmailerrorredIcon sx={{fill:"var(--danger)"}}/>
+  <span style={{color:"var(--danger)"}}>3</span>
+  </>)
+}
 
 export default MarketViewPage;
