314f002644f3dbe30c30e7c0955771278f65d154
lshlsh98 <lsh980324@naver.com>
2026-04-09 12:51:36 +0900

    1250

--- react_semi_back/src/main/java/kr/co/iei/mypage/model/service/MypageService.java
+++ react_semi_back/src/main/java/kr/co/iei/mypage/model/service/MypageService.java
@@ -10,9 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 import kr.co.iei.mypage.model.dao.MypageDao;
 import kr.co.iei.mypage.model.vo.BoardListRequestDto;
 import kr.co.iei.mypage.model.vo.BoardSummary;
+import kr.co.iei.mypage.model.vo.ChartResDto;
 import kr.co.iei.mypage.model.vo.CommentSummary;
 import kr.co.iei.mypage.model.vo.ReportRequestDto;
 import kr.co.iei.mypage.model.vo.ReportResponseDto;
+import kr.co.iei.mypage.model.vo.TradeStatusReqDto;
+import kr.co.iei.mypage.model.vo.TradeStatusResDto;
 import kr.co.iei.mypage.model.vo.UpdateCommentDto;
 import kr.co.iei.mypage.model.vo.UpdateDto;
 
@@ -142,6 +145,24 @@ public class MypageService {
 
 	public int findLikeDislikeCount(BoardListRequestDto request) {
 		int count = mypageDao.findLikeDislikeCount(request);
+		
+		return count;
+	}//
+
+	public List<ChartResDto> findChartAll(TradeStatusReqDto request) {
+		List<ChartResDto> chart = mypageDao.findChartAll(request); 
+		
+		return chart;
+	}//
+
+	public List<TradeStatusResDto> findListAll(TradeStatusReqDto request) {
+		List<TradeStatusResDto> list = mypageDao.findListAll(request);
+		
+		return list;
+	}//
+
+	public int findListAllCount(TradeStatusReqDto request) {
+		int count = mypageDao.findListAllCount(request);
 		
 		return count;
 	}//
