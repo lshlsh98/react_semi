6f92f1167b221f0d645485abf4685bb35ff57022
lshlsh98 <lsh980324@naver.com>
2026-04-14 11:05:02 +0900

    chat.config

---
+++ react_semi_back/src/main/java/kr/co/iei/chat/config/StompHandler.java
@@ -0,0 +1,66 @@
+package kr.co.iei.chat.config;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import kr.co.iei.chat.model.service.ChatService;
+
+@Component
+public class StompHandler implements ChannelInterceptor {
+	
+	@Value("${jwt.secret-key}")
+	private String secretKey;
+	
+	@Autowired
+	private ChatService chatService;
+	
+	@Override
+	public @Nullable Message<?> preSend(Message<?> message, MessageChannel channel) {
+		final StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+		
+		if(StompCommand.CONNECT == accessor.getCommand()) {
+			System.out.println("connect 요청 시 토큰 유효성 검증");
+			String token = accessor.getFirstNativeHeader("Authorization");
+			
+			// 토큰 검증
+			Jwts.parser()
+				.setSigningKey(secretKey)
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+			System.out.println("토큰 검증 완료");
+		}
+		
+		if(StompCommand.SUBSCRIBE == accessor.getCommand()) {
+			System.out.println("subscribe 검증");
+			String token = accessor.getFirstNativeHeader("Authorization");
+			
+			// 토큰 검증
+			Claims claims = Jwts.parser()
+				.setSigningKey(secretKey)
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+			
+			String memberId = claims.get("memberId", String.class);
+			String roomId = accessor.getDestination().split("/")[2];
+			if(!chatService.isRoomParticipant(memberId, Long.parseLong(roomId))) {
+				throw new AuthenticationServiceException("해당 room에 권한이 없습니다.");
+			}
+		}
+		
+		return message;
+	}//
+	
+	
+}
